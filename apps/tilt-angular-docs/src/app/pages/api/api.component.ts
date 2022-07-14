c82a8e346babc13564e01c813fe4f6ef0e037f0a
Jim Drury <jim.drury@virginmediao2.co.uk>
2022-07-14 11:16:36 +0100

    feat(tilt-angular-docs): adds search engine meta information to api docs

--- apps/tilt-angular-docs/src/app/pages/api/api.component.ts
+++ apps/tilt-angular-docs/src/app/pages/api/api.component.ts
@@ -1,8 +1,23 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Meta, Title} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-api',
   templateUrl: './api.component.html',
-  styleUrls: ['./api.component.css']
+  styleUrls: ['./api.component.css'],
 })
-export class ApiComponent {}
+export class ApiComponent implements OnInit {
+  constructor(
+    private title: Title,
+    private meta: Meta,
+  ) {
+  }
+
+  ngOnInit() {
+    this.title.setTitle('API documentation | Angular Tilt | Geometric Panda');
+    this.meta.addTag({
+      name: 'description',
+      content: 'API documentation to support the @geometricpanda/angular-tilt directives.'
+    })
+  }
+}
