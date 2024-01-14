233768f436683d61251a7bcf673a814edf3d5a11
Jim Drury <jim.drury@virginmediao2.co.uk>
2024-01-14 19:00:48 +0000

    chore: [nx migration] migration-v15

--- apps/tilt-angular-docs/src/app/pages/playground/playground.module.ts
+++ apps/tilt-angular-docs/src/app/pages/playground/playground.module.ts
@@ -1,11 +1,11 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {ReactiveFormsModule} from '@angular/forms';
-import {MatSliderModule} from '@angular/material/slider';
-import {MatCardModule} from '@angular/material/card';
-import {MatSlideToggleModule} from '@angular/material/slide-toggle';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
+import {MatLegacySliderModule as MatSliderModule} from '@angular/material/legacy-slider';
+import {MatLegacyCardModule as MatCardModule} from '@angular/material/legacy-card';
+import {MatLegacySlideToggleModule as MatSlideToggleModule} from '@angular/material/legacy-slide-toggle';
+import {MatLegacyFormFieldModule as MatFormFieldModule} from '@angular/material/legacy-form-field';
+import {MatLegacyInputModule as MatInputModule} from '@angular/material/legacy-input';
 
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import {NgTiltModule} from '@geometricpanda/angular-tilt';
